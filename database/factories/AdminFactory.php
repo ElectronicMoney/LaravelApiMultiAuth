3d4e3a90803c97f8eeba2143fdcb0046ed20259c
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-18 04:59:39 -0500

    Modeified the migrations to add api_token

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -8,6 +8,7 @@ $factory->define(App\Models\Admin::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => bcrypt('secret'), // secret
+        'api_token' => bin2hex(openssl_random_pseudo_bytes(30)),
         'remember_token' => str_random(10),
     ];
 });
